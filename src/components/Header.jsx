81fe5e6f32cb93d7373e11348d64f628a641d564
Izaan Ahmed Khan <izaanahmedkhan@gmail.com>
2023-08-29 19:08:44 +0500

    Header Link Fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ const getSearched = async () => {
     <div className="bg-white">
   <div className="border py-3 px-6">
     <div className="flex justify-between">
-      <Link to={`http://localhost:5173/`}>
+      <Link to={`/`}>
         <div className="flex items-center">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
